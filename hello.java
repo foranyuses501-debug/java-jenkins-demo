f81dcaf226b1bf4466a892560c4e73e8d670b315
foranyuses501-debug <foranyuses501@gmail.com>
2026-03-26 14:41:05 +0530

    Add hello.java to print message and sum two numbers

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main(String[] args) {
+        // Print message
+        System.out.println("Hello Jenkins");
+
+        // Add two numbers
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        // Print result
+        System.out.println("Sum: " + sum);
+    }
+}
